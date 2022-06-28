ff0cd338bbe5cfd46539e3cdf25380aad2a8e457
henrytam038 <henry.t@fivestones.net>
2022-06-28 09:52:41 +0800

    import datasource from config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,20 @@ import { ResultController } from './process/process.controller';
 import { MarketFeedController } from './marketFeed/marketFeed.controller';
 import { ProcessService } from './process/process.service';
 import { AppService } from './app.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { Result } from './entities/result.entity';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { ScheduleModule } from '@nestjs/schedule';
 import { join } from 'path';
 import { MarketFeedService } from './marketFeed/marketFeed.service';
+import { connectionSource } from './config/ormconfig';
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '123',
-      database: 'test',
-      entities: [join(__dirname, '**', '*.entities.{ts,js}')],
-      migrations: [join(__dirname, 'migrations', '*.{ts,js}')],
-      synchronize: false,
+    TypeOrmModule.forRootAsync({
+      useFactory: async () => {
+        return connectionSource.options as TypeOrmModuleOptions;
+      },
     }),
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'client', 'build'),
