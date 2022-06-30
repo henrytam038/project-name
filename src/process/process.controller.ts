3ec7f467782876dea4bedfbe26038e22715c49a5
henrytam038 <henry.t@fivestones.net>
2022-06-30 12:07:40 +0800

    change to Get() request

--- src/process/process.controller.ts
+++ src/process/process.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { ProcessService } from './process.service';
 import { UnderlyingData } from './dto/warrant-data.dto';
+import { AppEngineCronValidated } from '../auth/app-engine-cron-validated.decorator';
 
 @Controller('result')
+@AppEngineCronValidated()
 export class ResultController {
   constructor(private readonly processService: ProcessService) {}
 
-  @Post()
+  @Get()
   fetchDataAndStoreResult(): Promise<UnderlyingData[]> {
     return this.processService.fetchDataAndStoreResult();
   }
